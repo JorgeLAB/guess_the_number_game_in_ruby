5236c3ff2675a400e69c5b7da63350e9bdf0c7f3
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-04-08 00:01:21 -0300

    Adiciona dificuldade

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -10,9 +10,25 @@ def give_welcome
   puts 'We are begin the game for you, ' + nome.strip
 end
 
-def secret_number_draw
-  puts 'Choose a secret number between 0 and 200...'
-  secret_number = rand(1...200)
+def define_difficulty(difficulty)
+  case difficulty
+  when 1
+    max = 30
+  when 2
+    max = 60
+  when 3
+    max = 100
+  when 4
+    max = 150
+  else
+    max = 200
+  end
+end
+
+def secret_number_draw(difficulty)
+  max = define_difficulty(difficulty)
+  puts "Choose a secret number between 0 and #{max - 1}..."
+  secret_number = rand(max)
   puts "\nChosen... how about guessing our secret number today?\n"
   secret_number
 end
@@ -40,8 +56,14 @@ def verification_kicked(kicked, secret_number)
   end
 end
 
+def asks_for_difficulty
+  puts 'What level of difficulty do you want ? (1 ease, 5 difficulty)'
+  difficulty = gets.to_i
+end
+
 give_welcome
-secret_number = secret_number_draw
+difficulty = asks_for_difficulty
+secret_number = secret_number_draw(difficulty)
 
 kickes = []
 attempt_limit = 10
